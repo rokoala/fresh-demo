ba3140f19ae5166bda3ef2ea813f390bb54f6117
rokoala <koga.rodrigo@gmail.com>
2022-07-02 15:30:54 -0300

    feat(*): add examples

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,6 +1,14 @@
 /** @jsx h */
 import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
+import { PageProps, Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const resp = await ctx.render();
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};
 
 export default function Greet(props: PageProps) {
   return <div>Hello {props.params.name}</div>;
